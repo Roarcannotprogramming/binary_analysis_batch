7005d1ef39b2c3ac66980aff917e4fa24f780b9d
V1me <z1991998920@gmail.com>
2022-08-15 22:07:41 +0800

    test

--- test.ps1
+++ test.ps1
@@ -2,5 +2,5 @@ echo "Running test.ps1"
 echo 0 | .\ida75\idapyswitch.exe
 $env:PYTHONHOME="C:\hostedtoolcache\windows\Python\3.9.13\x64"
 ls .\ida75
-.\ida75\idat64.exe -B .\pwn3 2>&1
+.\ida75\idat64.exe -B .\pwn3 2>&1 | tee .\ida.log
 ls
