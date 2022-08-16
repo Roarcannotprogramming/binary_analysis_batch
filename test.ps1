5f6472b4a07e5969b0af70c488853a7d7e8a44c5
V1me <z1991998920@gmail.com>
2022-08-16 16:11:43 +0800

    test

--- test.ps1
+++ test.ps1
@@ -6,4 +6,4 @@ $env:PYTHONHOME="C:\hostedtoolcache\windows\Python\3.9.13\x64"
 New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 銆愯珛鏀寔姝ｇ増銆? Unlimited License' -Value "1" -PropertyType DWORD -Force
 # ls .\ida75
 .\ida75\idat64.exe -B .\pwn3 2>&1 | tee .\ida.log
-ls
+# ls
