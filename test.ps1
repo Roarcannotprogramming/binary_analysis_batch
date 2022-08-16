8143ac1ae166bb5e6cdc7336fc7b62afd261d371
V1me <z1991998920@gmail.com>
2022-08-16 15:48:47 +0800

    test

--- test.ps1
+++ test.ps1
@@ -1,9 +1,9 @@
 echo "Running test.ps1"
 whoami
 echo "TEST OUT"
-Write-Error "TEST ERROR" 
 echo 0 | .\ida75\idapyswitch.exe
 $env:PYTHONHOME="C:\hostedtoolcache\windows\Python\3.9.13\x64"
+New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 銆愯珛鏀寔姝ｇ増銆? Unlimited License' -Value "1" -PropertyType DWORD -Force
 # ls .\ida75
 .\ida75\idat64.exe -B .\pwn3 2>&1 | tee .\ida.log
 ls
