cd2994b7a26e4be0a852e38b1d2a07b8f184a227
V1me <z1991998920@gmail.com>
2022-08-16 20:05:02 +0800

    test

--- test.ps1
+++ test.ps1
@@ -3,8 +3,9 @@ echo "Running test.ps1"
 echo 1 | .\ida75\idapyswitch.exe
 $env:PYTHONHOME="C:\hostedtoolcache\windows\Python\3.9.13\x64"
 New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 銆愯珛鏀寔姝ｇ増銆? Unlimited License' -Value "1" -PropertyType DWORD -Force
-New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 【請支持正版�? Unlimited License' -Value "1" -PropertyType DWORD -Force
-New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 【請支持正版】, Unlimited License' -Value "1" -PropertyType DWORD -Force
+# New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 【請支持正版�? Unlimited License' -Value "1" -PropertyType DWORD -Force
+# New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 【請支持正版】, Unlimited License' -Value "1" -PropertyType DWORD -Force
+Get-ItemProperty -path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 銆愯珛鏀寔姝ｇ増銆? Unlimited License'
 Set-NetFirewallProfile -Profile Private -Enabled False
 Set-NetFirewallProfile -Profile Public -Enabled False
 # ls .\ida75
