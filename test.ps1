cb61aff261434838cc2e70d31b31a5b75c00b904
V1me <z1991998920@gmail.com>
2022-08-16 17:24:19 +0800

    test

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 echo "Running test.ps1"
 echo 0 | .\ida75\idapyswitch.exe
-$env:PYTHONHOME="C:\hostedtoolcache\windows\Python\3.9.13\x64"
+# $env:PYTHONHOME="C:\hostedtoolcache\windows\Python\3.9.13\x64"
 New-ItemProperty -Path 'HKCU:\SOFTWARE\Hex-Rays\IDA' -Name 'License Hex-Rays SA. 銆愯珛鏀寔姝ｇ増銆? Unlimited License' -Value "1" -PropertyType DWORD -Force
 Set-NetFirewallProfile -Profile Private -Enabled False
 Set-NetFirewallProfile -Profile Public -Enabled False
