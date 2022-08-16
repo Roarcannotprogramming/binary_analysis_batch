1c03ad105e6fb0d306319acc9af07074de043c20
V1me <z1991998920@gmail.com>
2022-08-16 13:26:03 +0800

    test

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 echo "Running test.ps1"
 whoami
 echo "TEST OUT"
-echo "TEST ERROR" 1>&2
+Write-Error "TEST ERROR" 
 echo 0 | .\ida75\idapyswitch.exe
 $env:PYTHONHOME="C:\hostedtoolcache\windows\Python\3.9.13\x64"
 # ls .\ida75
