371792b7a180e0edf917abc0b180e6a2cd4ce338
V1me <z1991998920@gmail.com>
2022-09-02 15:35:21 +0800

    new

--- analysis.sh
+++ analysis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # docker run --rm --env BINARY=pwn3 --name ida-docker -p 8080:8080 -v `pwd`:/root/host -it nyamisty/docker-wine-ida:7.5sp3
 cp /root/host/${BINARY} /root/.wine/drive_c/
-wine C:\\IDA\\ida64.exe -B C:\\${BINARY}
+timeout 18000 wine C:\\IDA\\ida64t.exe -B C:\\${BINARY}
 mv /root/.wine/drive_c/${BINARY}* /root/host
