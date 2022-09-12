b16c60609ea39ad3aac37ce18258f4673161a641
V1me <z1991998920@gmail.com>
2022-09-12 18:13:39 +0800

    new

--- analysis.sh
+++ analysis.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 # docker run --rm --env BINARY=pwn3 --name ida-docker -p 8080:8080 -v `pwd`:/root/host -it nyamisty/docker-wine-ida:7.5sp3
+
+watch_file() {
+    while true; do
+        if [ "$(cat /root/.wine/drive_c/ida_log | grep "The initial autoanalysis has been finished.")" ]; then
+            echo "[+] Already completed! Exit"
+            kill -s SIGTERM $$
+            exit 0
+        fi
+        sleep 60
+    done
+}
+
+do_save() {
+    echo "[*] Save"
+    mv /root/.wine/drive_c/${BINARY}* /root/host
+    mv /root/.wine/drive_c/ida_log /root/host
+    echo "[+] Save Done"
+}
+
+trap "do_save; exit 0" SIGINT SIGTERM
+
 echo "Starting IDA..."
-cp /root/host/* /root/.wine/drive_c/
+# cp /root/host/* /root/.wine/drive_c/
 find /root/host -type f | xargs cp - /root/.wine/drive_c/
 ls -alh /root/.wine/drive_c
-# cp /root/host/analysis.py /root/.wine/drive_c/
+watch_file &
 rm /root/.wine/drive_c/IDA/plugins/ipyida_plugin_stub.py
 wine C:\\IDA\\idat64.exe -L"C:\\ida_log" -A -S"C:\\analysis.py" C:\\${BINARY}
-mv /root/.wine/drive_c/${BINARY}* /root/host
-mv /root/.wine/drive_c/ida_log /root/host
-echo "Done"
+do_save
