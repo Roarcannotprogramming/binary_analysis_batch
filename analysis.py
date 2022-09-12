b93c2cec3c88a88a3e9cad3b276bb8c6ead553ec
V1me <z1991998920@gmail.com>
2022-09-12 19:17:24 +0800

    new

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from re import T
 import threading
 import ida_kernwin
 import ida_auto
@@ -8,14 +9,30 @@ import time
 
 
 def timout_thread_func():
-    print("Timeout thread started")
-    time.sleep(int(5.5*60*60))
-    print("Now! Analysis Timeout!")
+    print("[*] Timeout thread started")
+    # time.sleep(int(5.5*60*60))
+    for i in range(0, 5*60):
+        print("[*] Passed {} seconds".format(i))
+        time.sleep(1)
+    print("[+] Now! Analysis Timeout!")
     c = compile('ida_pro.qexit(0)', '', 'exec')
     ida_kernwin.execute_sync(lambda: exec(c), ida_kernwin.MFF_WRITE)
 
-t = threading.Thread(target=timout_thread_func)
-t.daemon = True
-t.start()
+def auto_ok_thread_func():
+    print("[*] Auto OK thread started")
+    while True:
+        if ida_kernwin.execute_sync(lambda: ida_auto.auto_is_ok(), ida_kernwin.MFF_WRITE):
+            print("[+] Auto analysis OK! Exit")
+            break
+        time.sleep(5)
+    c = compile('ida_pro.qexit(0)', '', 'exec')
+    ida_kernwin.execute_sync(lambda: exec(c), ida_kernwin.MFF_WRITE)
 
-print("Main thread started")
+timout_thread = threading.Thread(target=timout_thread_func)
+auto_ok_thread = threading.Thread(target=auto_ok_thread_func)
+timout_thread.daemon = True
+auto_ok_thread.daemon = True
+timout_thread.start()
+auto_ok_thread.start()
+
+print("[*] Analysis begin...")
--- analysis.sh
+++ analysis.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # docker run --rm --env BINARY=pwn3 --name ida-docker -p 8080:8080 -v `pwd`:/root/host -it nyamisty/docker-wine-ida:7.5sp3
 
-watch_file() {
-    while true; do
-        if [ "$(cat /root/.wine/drive_c/ida_log | grep "The initial autoanalysis has been finished.")" ]; then
-            echo "[+] Already completed! Exit"
-            kill -s SIGTERM $$
-            exit 0
-        fi
-        sleep 60
-    done
-}
+# watch_file() {
+#     while true; do
+#         if [ "$(cat /root/.wine/drive_c/ida_log | grep "The initial autoanalysis has been finished.")" ]; then
+#             echo "[+] Already completed! Exit"
+#             kill -s SIGTERM $$
+#             exit 0
+#         fi
+#         sleep 60
+#     done
+# }
 
 do_save() {
     echo "[*] Save"
@@ -19,16 +19,17 @@ do_save() {
     echo "[+] Save Done"
 }
 
-trap "do_save; exit 0" SIGINT SIGTERM
+# trap "do_save; exit 0" SIGINT SIGTERM
 
 echo "Starting IDA..."
+echo ${BINARY}
 # cp /root/host/* /root/.wine/drive_c/
 # find /root/host -type f | xargs cp /root/.wine/drive_c/
-for f in $(find /root/host -type f); do
+for f in $(find /root/host -type f -maxdepth 1); do
     cp $f /root/.wine/drive_c/
 done
 ls -alh /root/.wine/drive_c
-watch_file &
+# watch_file &
 rm /root/.wine/drive_c/IDA/plugins/ipyida_plugin_stub.py
 wine C:\\IDA\\idat64.exe -L"C:\\ida_log" -A -S"C:\\analysis.py" C:\\${BINARY}
 do_save
